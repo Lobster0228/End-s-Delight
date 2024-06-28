7e41ca44647e5a3490677d42fd6944e87c64dfb7
unilock <unilock@fennet.rentals>
2024-06-28 12:32:28 -0400

    Simplify Dragon Tooth Knife event + mixin
    Fixes https://github.com/FoggyHillside/End-s-Delight/issues/33 + https://github.com/FoggyHillside/End-s-Delight/issues/35
    
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cn/foggyhillside/ends_delight/mixin/LivingEntityMixin.java
+++ src/main/java/cn/foggyhillside/ends_delight/mixin/LivingEntityMixin.java
@@ -2,60 +2,52 @@ package cn.foggyhillside.ends_delight.mixin;
 
 import cn.foggyhillside.ends_delight.item.DragonToothKnifeEvent;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.entity.Attackable;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin({LivingEntity.class})
-public abstract class LivingEntityMixin extends Entity implements Attackable {
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin {
 
     @Shadow
     public abstract float getHealth();
 
-    public LivingEntityMixin(EntityType<?> type, World world) {
-        super(type, world);
-    }
-
     @ModifyArg(
-            method = {"applyDamage"},
+            method = "applyDamage",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/entity/LivingEntity;setHealth(F)V"
             ),
             index = 0
     )
-    protected float setHealthMixin(float amount, @Local DamageSource source) {
+    protected float setHealthMixin(float amount, @Local(argsOnly = true) DamageSource source) {
         return this.getHealth() - DragonToothKnifeEvent.KnifeEvents.onAttackEndMobs((LivingEntity) (Object) this, source, this.getHealth() - amount);
     }
 
     @ModifyArg(
-            method = {"applyDamage"},
+            method = "applyDamage",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/entity/damage/DamageTracker;onDamage(Lnet/minecraft/entity/damage/DamageSource;F)V"
             ),
             index = 1
     )
-    protected float onDamageMixin(float amount, @Local DamageSource source) {
+    protected float onDamageMixin(float amount, @Local(argsOnly = true) DamageSource source) {
         return this.getHealth() - DragonToothKnifeEvent.KnifeEvents.onAttackEndMobs((LivingEntity) (Object) this, source, this.getHealth() - amount);
     }
 
     @ModifyArg(
-            method = {"applyDamage"},
+            method = "applyDamage",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/entity/LivingEntity;setAbsorptionAmount(F)V"
             ),
             index = 0
     )
-    protected float setAbsorptionAmountMixin(float amount, @Local DamageSource source) {
+    protected float setAbsorptionAmountMixin(float amount, @Local(argsOnly = true) DamageSource source) {
         return this.getHealth() - DragonToothKnifeEvent.KnifeEvents.onAttackEndMobs((LivingEntity) (Object) this, source, this.getHealth() - amount);
     }
 
